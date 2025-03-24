9a5d3ba2f19d5fcc653e028308bf66d7b445e398
Ken Miller <kmillerjr@gmail.com>
2025-03-24 12:03:30 +0000

    update config to run in the challenge environment

--- code-challenge/exercise-1/config.py
+++ code-challenge/exercise-1/config.py
@@ -1,9 +1,9 @@
 import os
 
 # Redis connection settings with environment variable support
-REDIS_SOURCE_HOST = os.getenv('REDIS_SOURCE_HOST', 'localhost')
+REDIS_SOURCE_HOST = os.getenv('REDIS_SOURCE_HOST', '172.16.22.21')
 REDIS_SOURCE_PORT = int(os.getenv('REDIS_SOURCE_PORT', '13300'))
-REDIS_REPLICA_HOST = os.getenv('REDIS_REPLICA_HOST', 'localhost')
+REDIS_REPLICA_HOST = os.getenv('REDIS_REPLICA_HOST', '172.16.22.22')
 REDIS_REPLICA_PORT = int(os.getenv('REDIS_REPLICA_PORT', '19777'))
 REDIS_SOURCE_PASSWORD = os.getenv('REDIS_SOURCE_PASSWORD')
 REDIS_REPLICA_PASSWORD = os.getenv('REDIS_REPLICA_PASSWORD') 
